5ed4150236ecd1cb14d9ce885dcb0adffc957fef
TS-coordinator <118684325+TS-coordinator@users.noreply.github.com>
2023-05-30 23:54:24 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import { AppInfo } from "@/types/app"
 export const APP_ID = '6b1c2f5b-13df-4e3b-bf2b-ff6c3961875a'
 export const API_KEY = '<Web API Key From Dify>'
-export const API_URL = 'https://cloud.dify.ai/app'
+export const API_URL = ''
 export const APP_INFO: AppInfo = {
   "title": 'Chat APP',
   "description": '这是一个旅游百事通，避坑指南。你可以依靠 AI 做旅游攻略，查询门票、设计路线日程、了解目的地的人文历史，让你的旅游更方便，更有意义。',
