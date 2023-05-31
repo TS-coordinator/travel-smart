e7993f63792af79f9ec6f6b1314ce47612fab1d8
TS-coordinator <118684325+TS-coordinator@users.noreply.github.com>
2023-05-31 20:53:05 +0800

    Update index.ts
    APP_URL = 'travel-smart-g7v4gjfxp-tenderstar.vercel.app'

--- config/index.ts
+++ config/index.ts
@@ -2,6 +2,7 @@ import { AppInfo } from "@/types/app"
 export const APP_ID = '6b1c2f5b-13df-4e3b-bf2b-ff6c3961875a'
 export const API_KEY = '<Web API Key From Dify>'
 export const API_URL = 'https://api.dify.ai/v1'
+export const APP_URL = 'travel-smart-g7v4gjfxp-tenderstar.vercel.app'
 export const APP_INFO: AppInfo = {
   "title": 'Chat APP',
   "description": '这是一个旅游百事通，避坑指南。你可以依靠 AI 做旅游攻略，查询门票、设计路线日程、了解目的地的人文历史，让你的旅游更方便，更有意义。',
@@ -19,8 +20,8 @@ export const LOCALE_COOKIE_NAME = 'locale'
 
 export const DEFAULT_VALUE_MAX_LEN = 48
 
-export const WEB_API_CORS_ALLOW_ORIGINS: '*'
-export const CONSOLE_CORS_ALLOW_ORIGINS: '*'
-export const COOKIE_HTTPONLY: 'true'
-export const COOKIE_SAMESITE: 'Lax'
-export const COOKIE_SECURE: 'false'
+export const WEB_API_CORS_ALLOW_ORIGINS: =  '*'
+export const CONSOLE_CORS_ALLOW_ORIGINS: =  '*'
+export const COOKIE_HTTPONLY: = 'true'
+export const COOKIE_SAMESITE: = 'Lax'
+export const COOKIE_SECURE: = 'false'
